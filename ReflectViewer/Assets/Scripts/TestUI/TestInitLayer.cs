901cec3c710d584ef69529f3d36ee8fafcfa999f
sergoutback <webart2013@gmail.com>
2022-08-18 20:07:30 +0300

    18082022_2006
    Need to fix instantiate layer

--- ReflectViewer/Assets/Scripts/TestUI/TestInitLayer.cs
+++ ReflectViewer/Assets/Scripts/TestUI/TestInitLayer.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class TestInitLayer : MonoBehaviour
 {
-    [SerializeField] private GameObject layer;
+    public GameObject layer;
 
     private string _layerName;
 
     private bool _checkbox;
 
-    public Dictionary<bool, string> layerDictionary;
+    //public Dictionary<bool, string> layerDictionary;
 
-    //public string LayerName;
+    public string LayerName;
 
-    //public bool Checkbox;
+    public bool Checkbox;
 
 
 
@@ -35,13 +35,17 @@ public class TestInitLayer : MonoBehaviour
     {
         for (int i = 0; i < 10; i++)
 
-            LayerStart(_checkbox, _layerName);
+            LayerStart(Checkbox, LayerName);
     }
-
+     
 
     public void LayerStart(bool _checkbox, string _layerName)
     {
-        if (_checkbox)
+        LayerName = _layerName;
+
+        Checkbox = _checkbox;
+
+        if (Checkbox)
 
             layer = Instantiate(layer, new Vector3(0, transform.position.y - 60, 0), Quaternion.identity);
 
